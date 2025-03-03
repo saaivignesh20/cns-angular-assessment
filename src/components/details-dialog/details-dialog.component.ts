310497d37bf1f988d1395f7bd5df9479e6e81ae9
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-03-03 18:40:26 -0500

    remove unneeded spec files

--- src/components/details-dialog/details-dialog.component.ts
+++ src/components/details-dialog/details-dialog.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { MatDialogClose, MatDialogContent, MAT_DIALOG_DATA, MatDialogActions } from '@angular/material/dialog';
+import { NgIf } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton, MatIconButton } from '@angular/material/button';
-import { NgIf } from '@angular/common';
+import { MatDialogClose, MatDialogContent, MAT_DIALOG_DATA, MatDialogActions } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-details-dialog',
